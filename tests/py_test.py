4afa74e64ee203bf76af6b4e58bbefbe6aa8a9f0
quyentx <quyentx@outlook.com>
2020-09-01 15:41:16 +0700

    add one false test

--- tests/py_test.py
+++ tests/py_test.py
@@ -10,3 +10,9 @@ class GoogleSearch(TestBase):
     def test_google_search(self):
         google_home_page = GoogleHomePage(self.driver)
         google_home_page.search()
+        assert True
+
+    def test_google_search2(self):
+        google_home_page = GoogleHomePage(self.driver)
+        google_home_page.search()
+        assert False
